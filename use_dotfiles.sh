c815f003517a3ad9af49487fb2a1e4a2332609de
Ken Lauer <klauer@slac.stanford.edu>
2022-03-01 10:01:56 -0800

    MNT: more useful use_dotfiles script

--- use_dotfiles.sh
+++ use_dotfiles.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -xe
-
 cd $HOME
 
 if [ ! -d dotfiles ]; then
@@ -9,12 +7,25 @@ if [ ! -d dotfiles ]; then
     exit 1;
 fi
 
-[ ! -f .bashrc ] && ln -s dotfiles/bashrc .bashrc
-[ ! -f .bash_aliases ] && ln -s dotfiles/bash_aliases .bash_aliases
-[ ! -f .bash_profile ] && ln -s dotfiles/bash_profile .bash_profile
-[ ! -f .vimrc ] && ln -s dotfiles/vimrc .vimrc
-[ ! -f .condarc ] && ln -s dotfiles/condarc .condarc
-[ ! -f .gitconfig ] && ln -s dotfiles/gitconfig .gitconfig
+install_file() {
+    source_file=$1
+    destination_file=$2
+    if [ ! -e "$destination_file" ]; then
+        echo "Soft-linking $destination_file to $source_file..."
+        ln -s "$source_file" "$destination_file"
+    else
+        echo "!! Cannot install $1 to $HOME/$2 as it already exists."
+        echo "   Please back up or remove your existing file."
+        ls -l "$HOME/$destination_file"
+    fi
+}
+
+install_file dotfiles/bashrc .bashrc
+install_file dotfiles/bash_aliases .bash_aliases
+install_file dotfiles/bash_profile .bash_profile
+install_file dotfiles/vimrc .vimrc
+install_file dotfiles/condarc .condarc
+install_file dotfiles/gitconfig .gitconfig
 
 mkdir -p .ssh
-[ ! -f .ssh/config ] && ln -s dotfiles/ssh/config .ssh/config
+install_file dotfiles/ssh/config .ssh/config
