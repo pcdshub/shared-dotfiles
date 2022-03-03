cf334fa3a5df27b81d9907459b71361a28c719fd
Ken Lauer <klauer@slac.stanford.edu>
2022-03-02 17:01:39 -0800

    MNT: note the existing config as a remote config

--- use_dotfiles.sh
+++ use_dotfiles.sh
@@ -29,4 +29,4 @@ install_file dotfiles/tmux.conf .tmux.conf
 install_file dotfiles/vimrc .vimrc
 
 mkdir -p .ssh
-install_file dotfiles/ssh/config .ssh/config
+install_file dotfiles/ssh/pcds_config .ssh/config
