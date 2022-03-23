1e2980bf0aea8e6a5e4d7863064be124c7931f45
Ken Lauer <klauer@slac.stanford.edu>
2022-03-23 14:35:15 -0700

    MNT: split on/off config for some files

--- use_dotfiles.sh
+++ use_dotfiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd "$HOME" || exit
+cd "$HOME" || (echo "Unable to cd to $HOME for some reason"; exit 1)
 
 if [ ! -d "dotfiles" ]; then
     echo "Did you forget to clone dotfiles into $HOME/dotfiles?" > /dev/stderr
@@ -33,9 +33,39 @@ link_file() {
     fi
 }
 
-link_file dotfiles/bash_aliases .bash_aliases
-link_file dotfiles/bash_functions .bash_functions
-link_file dotfiles/bash_profile .bash_profile
+cat <<EOF
+* Do you want to install the on-site or off-site configuration?
+
+Example machines to use the on-site configuration with:
+* pslogin
+* psbuild-rhel7
+* Other shared PCDS machines with .slac.stanford.edu in the hostname
+
+Example machines to use the off-site configuration with:
+* Your laptop, sitting at home
+* Your laptop, which moves around the lab
+* Your home computer
+* Your personal laptop
+
+Your host is: $(hostname)
+
+Please choose from: "on" or "off"
+EOF
+
+read -r on_or_off
+
+if [[ $on_or_off == "on" ]]; then
+    echo "Installing the on-site configuration."
+elif [[ $on_or_off == "off" ]]; then
+    echo "Installing the off-site configuration."
+else
+    echo "Invalid choice '$on_or_off'; exiting."
+    exit 1
+fi
+
+link_file "dotfiles/${on_or_off}_site/bash_aliases" .bash_aliases
+link_file "dotfiles/${on_or_off}_site/bash_functions" .bash_functions
+link_file "dotfiles/${on_or_off}_site/bash_profile" .bash_profile
 link_file dotfiles/bashrc .bashrc
 link_file dotfiles/condarc .condarc
 link_file dotfiles/gitconfig .gitconfig
