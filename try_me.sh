1ff72589af95717182c95883c2246bb06a9f2e7b
Ken Lauer <klauer@slac.stanford.edu>
2022-04-15 14:23:05 -0700

    MNT: better try_me script

--- try_me.sh
+++ try_me.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "To try these dotfiles without installing them, run the following: "
-echo "$ bash --rcfile try_me.sh"
+
+if [ "$0" != "bash" ]; then
+    echo "$ bash --rcfile try_me.sh"
+    echo
+    echo "If you find your existing settings are getting in the way of trying shared-dotfiles,"
+    echo "try using the following instead:"
+    # shellcheck disable=SC2016
+    echo '$ env -i - TERM=$TERM HOME=$HOME XDG_SESSION_ID=$XDG_SESSION_ID XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR SSH_AUTH_SOCK=$SSH_AUTH_SOCK DISPLAY=$DISPLAY bash --rcfile try_me_onsite.sh --noprofile'
+    exit 1;
+fi
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -13,3 +21,5 @@ echo "* Loading the provided bash_functions..."
 source "$SCRIPT_DIR/on_site/bash_functions"
 echo "* Loading the provided bash_aliases..."
 source "$SCRIPT_DIR/on_site/bash_aliases"
+
+export PS1="[dotfiles-tryme] $PS1"
