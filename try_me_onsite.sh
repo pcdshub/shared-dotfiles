5622d38a90de5fe7130f420be72ac87434567e8f
Ken Lauer <klauer@slac.stanford.edu>
2022-04-18 14:07:08 -0700

    MNT: tweak try-me text

--- try_me_onsite.sh
+++ try_me_onsite.sh
@@ -2,10 +2,16 @@
 
 
 if [ "$0" != "bash" ]; then
+    echo "To try the bash configuration provided here, run the following:"
     echo "$ bash --rcfile try_me_onsite.sh"
     echo
-    echo "If you find your existing settings are getting in the way of trying shared-dotfiles,"
-    echo "try using the following instead:"
+    echo "Please note that this does not include the ssh configuration, vim configuration,"
+    echo "or anything beyond the bash settings."
+    echo
+    echo "If you find your existing settings are getting in the way of trying shared-dotfiles"
+    echo "you can start a new bash session that is isolated from your configuration by way of "
+    echo "the following:"
+    echo
     # shellcheck disable=SC2016
     echo '$ env -i - USER=$USER TERM=$TERM HOME=$HOME XDG_SESSION_ID=$XDG_SESSION_ID XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR SSH_AUTH_SOCK=$SSH_AUTH_SOCK DISPLAY=$DISPLAY bash --rcfile try_me_onsite.sh --noprofile'
     exit 1;
