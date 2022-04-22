710aee67573ae0f719b47517a4b84ec402cff42a
Ken Lauer <klauer@slac.stanford.edu>
2022-04-22 14:22:18 -0700

    REF: alternative try_me that's easier

--- try_me_onsite.sh
+++ try_me_onsite.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-
-if [ "$0" != "bash" ]; then
-    echo "To try the bash configuration provided here, run the following:"
-    echo "$ bash --rcfile try_me_onsite.sh"
-    echo
-    echo "Please note that this does not include the ssh configuration, vim configuration,"
-    echo "or anything beyond the bash settings."
-    echo
-    echo "If you find your existing settings are getting in the way of trying shared-dotfiles"
-    echo "you can start a new bash session that is isolated from your configuration by way of "
-    echo "the following:"
-    echo
-    # shellcheck disable=SC2016
-    echo '$ env -i - USER=$USER TERM=$TERM HOME=$HOME XDG_SESSION_ID=$XDG_SESSION_ID XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR SSH_AUTH_SOCK=$SSH_AUTH_SOCK DISPLAY=$DISPLAY bash --rcfile try_me_onsite.sh --noprofile'
-    exit 1;
-fi
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 export dotfiles="$SCRIPT_DIR"
+
+if [ "$_SHARED_DOTFILE_TRIAL" == "" ]; then
+    cd "$SCRIPT_DIR" || (echo "Failed to cd to shared-dotfiles." && exit 1)
+    echo "Running $SCRIPT_DIR"
+    exec env -i - _SHARED_DOTFILE_TRIAL=1 \
+            USER="$USER" \
+            TERM="$TERM" \
+            HOME="$HOME" \
+            XDG_SESSION_ID="$XDG_SESSION_ID" \
+            XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" \
+            SSH_AUTH_SOCK="$SSH_AUTH_SOCK" \
+            DISPLAY="$DISPLAY" \
+            dotfiles="$dotfiles" \
+        bash --rcfile "try_me_onsite.sh" --noprofile
+    exit
+fi
 
 echo "* Loading the provided bashrc..."
 source "$SCRIPT_DIR/on_site/bashrc"
@@ -28,4 +26,20 @@ source "$SCRIPT_DIR/on_site/bash_functions"
 echo "* Loading the provided bash_aliases..."
 source "$SCRIPT_DIR/on_site/bash_aliases"
 
+# Custom settings just for "tryme" mode:
 export PS1="[dotfiles-tryme] $PS1"
+
+# Alias some tools to allow for the configuration to be used
+alias vi='vi -u "$dotfiles/vimrc"'
+alias vim='vim -u "$dotfiles/vimrc"'
+alias view='view -u "$dotfiles/vimrc"'
+
+echo "
+Loaded the shared dotfiles.
+For a reminder on what is provided with this package, take a look at
+'helpme.md' in the editor of your choice or type:
+
+    $ helpme
+
+When done testing, type 'exit' to return to your usual bash prompt.
+"
